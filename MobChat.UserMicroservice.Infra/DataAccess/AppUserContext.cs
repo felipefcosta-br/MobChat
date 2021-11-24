0cb67afc9a210f1fd2901a01dd5c0f9ed0e40f94
Felipe Faria da Costa <felipefcosta.br@outlook.com>
2021-11-24 13:20:05 +0100

    Update AppUserContext.cs

--- MobChat.UserMicroservice.Infra/DataAccess/AppUserContext.cs
+++ MobChat.UserMicroservice.Infra/DataAccess/AppUserContext.cs
@@ -13,7 +13,7 @@ namespace MobChat.UserMicroservice.Infra.DataAccess
 
         public AppUserContext()
         {
-            this.dbConnectionString = "Server=tcp:mobchat-user-db-server.database.windows.net,1433;Initial Catalog=mobchat-usermicroservice-db;Persist Security Info=False;User ID=felipefcrj;Password=00Xam-Project-BR-77;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            this.dbConnectionString = "Server=tcp:Connection Timeout=30;";
             Database.EnsureCreated();
         }
 
